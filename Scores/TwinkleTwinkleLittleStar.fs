7aa0b3a99296ff8166f67594d506d4d749a2a328
Charles Zhang <szinubuntu@gmail.com>
2023-10-12 15:29:24 -0500

    [Score] Complete Twinkle Twinkle Little Star

--- Scores/TwinkleTwinkleLittleStar.fs
+++ Scores/TwinkleTwinkleLittleStar.fs
@@ -1,13 +1,13 @@
 Mode: Multi-Instrument
 # Tempo 120
 # Time Signature 4/4
-(120) 4/4
+(100) 4/4
 
 Piano 1:Piano [C C G G] [A A G/2] [F F E E] [D D C/2]
-Piano 2:Piano [C C G G] [A A G/2] [F F E E] [D D C/2]
+Piano 2:Piano [C3 G3 E3 G3] [F3 A3 C3 G3] [G2 B2 C3 F3] [G2 B2 C3|E3/2]
 
 Piano 1:Piano [G G F F] [E E D/2] [G G F F] [E E D/2]
-Piano 2:Piano [G G F F] [E E D/2] [G G F F] [E E D/2]
+Piano 2:Piano [E3 G3 D3 G3] [C3 G3 B2 G3] [E3 G3 D3 G3] [C3 G3 B2 G2]
 
 Piano 1:Piano [C C G G] [A A G/2] [F F E E] [D D C/2]
-Piano 2:Piano [C C G G] [A A G/2] [F F E E] [D D C/2]
+Piano 2:Piano [C3 C3 E3 G3] [F3 A3 C3 G3] [G2 B2 C3 E3] [G2 B2 C3|E3/2]
