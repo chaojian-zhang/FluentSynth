aad0e24d2ea6c171cb9f7b2cb27a2964cbeaa783
Charles Zhang <szinubuntu@gmail.com>
2023-10-12 20:40:24 -0500

    [Score] Create "Castle in the Sky"
    - Organize existing score

---
+++ Scores/NumberNotation/CastleInTheSky.fs
@@ -0,0 +1,16 @@
+# Castle in the Sky (天空之城) 天空の城ラピュタ
+# Music composed by Joe Hisaishi
+# Transcription into Fluent Synth by Charles Zhang, 2023 Oct 12th
+(87) 4/4
+
+Piano [- - 6_ 7_] [1. 7_/8 1 3] [7_/2. 3_] [6_. 5_/8 6_ 1] [5_/2. 3_]
+Piano [4_. 3_/8 4_/8 1.] [3_/2. 1] [7_. 4_/8 4_ 7_] [7_/2. 6_/8 7_/8] [1. 7_/8 1 3]
+Piano [7_/2. 3_/8 3_/8] [6_. 5_/8 6_ 1] [5_/2. 3_] [4_ 1/8 7_. 1]
+Piano [2 3/8 1...] [1/8 7_/8 6_ 7_ 5_] [6_/2. 1/8 2/8] [3. 2/8 3 5]
+Piano [2/2. 5_] [1. 7_/8 1 2/8 3/8] [3/1] [6_/8 7_/8 1 7_/8 1/8 2]
+Piano [1. 5_/8 5_/2] [4 3 2 1] [3/2. 3] [6. 6/8 5. 5/8]
+Piano [3/8 2/8 1 1/2] [2. 1/8 2 5] [3/2. 3] [6. 6/8 5. 5/8]
+Piano [3/8 2/8 1 1/2] [2. 1/8 2 7_] [6_/2. -]
+
+# TODO: pending implements 4#_ and 5# notes.
+# Source: https://www.qinyipu.com/jianpu/chunyinle/2181.html
