0d84e55ea61dc4143d297d1bf334a90cd9eaf7b4
Charles Zhang <szinubuntu@gmail.com>
2024-08-04 11:32:53 -0400

    Drafts

---
+++ Samples/20231013_Draft1.fs
@@ -0,0 +1,49 @@
+(150) [G_/8 G_/8 E_/8 E_/8 B_/4 B_/4]
+G_
+E_
+B_
+C_
+c
+g
+e
+b
+e
+e g b b
+e g b - b b 
+e g b - b g - g d
+e g b - b g - g d - e g b - g b - g d -
+e g b
+e g b d f a c
+e g b - f a c
+e g b - f a e
+e g b - f a f
+e g b - f a b
+e g b - f a d
+e g b - f a d'
+e g b - f a c'
+e g b - b g - g d - e g b - g b - g d - 
+e g b - b g - g d - e g b - g b - f a c'
+e g b - b g - g d - e g b - g b - d' f' a'
+e g b - b g - g d - e g b - g b - d' e' g'
+e g b - b g - g d - e g b - g b - d' e' g'
+e g b - b g - g d - e g b - g b - d' e' e'
+e g b - b g - g d - e g b - g b - b d' d'
+e g b - b g - g d - e g b - g b - b d' d'
+e g b - b g - g d - e g b - g b - b d' d' - b/2 d' d' 
+e g b - b g - g d - e g b - g b - b d' d' - b/2 d' d' - d' b/2 g
+e g b - b g - g d - e g b - g b - b d' d' - b/2 d' d' - d' b/2
+e g b - b g - g d - e g b - g b - b d' d' - b/2 d' d' - - d' b/2 g
+- b/2 d' d' - - d' b/2 g
+- b/2 d' d' - - d' - b/2 d' b g
+- b/2 d' d' - - d' - b/8 d' b/8 g
+- b/2 d' d' - - d' -. b/8 d' b/8 g
+- b/2 d' d' - - d' - b/8 -/8 d' b/8 g
+- b/2 d' d' - - d' - b/8 d'. b/8 g
+- b/2 d' d' - - d' - b/8 d'. -/8 b/8 g
+- b/2 d' d' - - d' - b d'. -/8 b/8 g
+- b/2 d' d' - - d' - b d'. -/8 b/8 g g - g b g -
+[- b/2 d' d'] [- - d' -] [b d'. -/8 b/8] [g g - g] [b g -]
+- b/2 d' d' - - d' - b d'. -/8 b/8 g g. -/8 g b g -
+- b/2 d' d' - - d' - b d'. -/8 b/8 g g. -/16 g b g -
+- b/2 d' d' - - d' - b d'. -/8 b/8 g g. -/2 g b g -
+[- b/2 d' d'] [- - d' -] [b d'. -/8] [b/8 g g.] [-/2 g b g] [-/1]
